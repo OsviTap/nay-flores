8d710cf0fc89bb3bc0a8a6e4e30dd719ed7c6409
Johan Orellana <johan.orellana.tapia@gmail.com>
2026-09-21 19:32:27 -0400

    fix: more horizontal padding on content

--- src/components/MessageModal.tsx
+++ src/components/MessageModal.tsx
@@ -222,7 +222,7 @@ export function MessageModal({ isOpen, onClose }: MessageModalProps) {
               </button>
             </header>
 
-            <main className="relative z-10 px-6 py-8 sm:px-10 sm:py-10">
+            <main className="relative z-10 px-8 py-8 sm:px-12 sm:py-10">
               <AnimatePresence mode="wait">
                 <motion.article
                   key={currentPage}
